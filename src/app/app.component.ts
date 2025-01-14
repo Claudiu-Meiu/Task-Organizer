2af86d8189a7225d9de0aa89b7c2d57d8d6e58ec
Claudiu <claudiu-meiu@proton.me>
2025-01-15 01:45:17 +0200

    Deleted header component; Changed styling for sidebar component; Project formatted with prettier.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, BoardComponent],
+  imports: [RouterOutlet, SidebarComponent, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
